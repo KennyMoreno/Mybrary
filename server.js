de4d14a9f0da56b034180ec53e745dd3221ba54b
KennyMoreno <zealousprogrammer@gmail.com>
2019-08-26 16:48:27 -0700

    Initial router routes

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express')
 const app = express();
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views')
@@ -13,6 +15,7 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 // Database Connection
 const mongoose = require('mongoose')
@@ -25,4 +28,6 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+
 app.listen(process.env.PORT || 3000)
